15a0a47df6c193cb4c0db0334a1d0513a4a4edd8
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-17 16:23:27 -0700

    Pizza Legends Part 13 - Battle System Part 3 - Status Effects

--- Battle/BattleEvent.js
+++ Battle/BattleEvent.js
@@ -20,7 +20,12 @@ class BattleEvent {
   }
 
   async stateChange(resolve) {
-    const { caster, target, damage } = this.event;
+    const { caster, target, damage, recover, status, action } = this.event;
+    let who = this.event.onCaster ? caster : target;
+    if (action.targetType === 'friendly') {
+      who = caster;
+    }
+
     if (damage) {
       //modify the target to have less HP
       target.update({
@@ -29,6 +34,27 @@ class BattleEvent {
 
       //start blinking
       target.pizzaElement.classList.add('battle-damage-blink');
+    }
+
+    if (recover) {
+      let newHp = who.hp + recover;
+      if (newHp > who.maxHp) {
+        newHp = who.maxHp;
+      }
+      who.update({
+        hp: newHp,
+      });
+    }
+
+    if (status) {
+      who.update({
+        status: { ...status },
+      });
+    }
+    if (status === null) {
+      who.update({
+        status: null,
+      });
     }
 
     //Wait a little bit
--- Battle/TurnCycle.js
+++ Battle/TurnCycle.js
@@ -20,7 +20,9 @@ class TurnCycle {
       caster,
       enemy,
     });
-    const resultingEvents = submission.action.success;
+
+    const resultingEvents = caster.getReplacedEvents(submission.action.success);
+
     for (let i = 0; i < resultingEvents.length; i++) {
       const event = {
         ...resultingEvents[i],
@@ -30,6 +32,26 @@ class TurnCycle {
         target: submission.target,
       };
       await this.onNewEvent(event);
+    }
+
+    //Check for post events
+    //(Do things AFTER your original turn submission)
+    const postEvents = caster.getPostEvents();
+    for (let i = 0; i < postEvents.length; i++) {
+      const event = {
+        ...postEvents[i],
+        submission,
+        action: submission.action,
+        caster,
+        target: submission.target,
+      };
+      await this.onNewEvent(event);
+    }
+
+    //Check for status expire
+    const expiredEvent = caster.decrementStatus();
+    if (expiredEvent) {
+      await this.onNewEvent(expiredEvent);
     }
 
     this.currentTeam = this.currentTeam === 'player' ? 'enemy' : 'player';
